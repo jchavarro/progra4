f83d9e86abf6fe9e7a04255546cd23e893744292
jchavarro <42480514+jchavarro@users.noreply.github.com>
2019-01-31 22:50:19 -0500

    Add files via upload

---
+++ Puzzle/src/puzzle/Puzzle.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package puzzle;
+
+/**
+ *
+ * @author JuanCaCha
+ */
+public class Puzzle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VistaPuzzle ventana = new VistaPuzzle();
+        ventana.setVisible(true);
+        
+    }
+    
+}
